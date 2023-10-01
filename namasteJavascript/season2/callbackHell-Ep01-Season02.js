9361944b9cdb4e03a93e3aa7fb76de567f429ef9
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-10-01 12:58:19 -0400

    callback

---
+++ namasteJavascript/season2/callbackHell-Ep01-Season02.js
@@ -0,0 +1,7 @@
+//JS is a synchronous single threaded language
+//callback is a way to write asynchronous code
+//callback hell is called pyramid of doom
+
+setTimeout(() => {
+    console.log('JavaScript')
+}, 1000)
