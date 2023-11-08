084e68a18e9398fe4a0dcb5b8db9d6586058ad8d
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-11-08 18:05:58 -0500

    prettierrc added

--- namasteJavascript/season2/async-await-part4.js
+++ namasteJavascript/season2/async-await-part4.js
@@ -1,29 +1,27 @@
-
 //resolving Promise, using async-await, more complex problem
 
 const promise1 = new Promise((resolve) => {
-    setTimeout(() => {
-        resolve('Promise1 is resolved')
-    }, 6000)
+  setTimeout(() => {
+    resolve('Promise1 is resolved')
+  }, 6000)
 })
 
 const promise2 = new Promise((resolve) => {
-    setTimeout(() => {
-        resolve('Promise2 is resolved')
-    }, 3000)
+  setTimeout(() => {
+    resolve('Promise2 is resolved')
+  }, 3000)
 })
 
-
 async function getData() {
-    console.log('Start execution')
-    
-    const val1 = await promise1;
-    console.log('Subrato')
-    console.log(val1)
+  console.log('Start execution')
 
-    const val2 = await promise2;
-    console.log('Mouli')
-    console.log(val2)
+  const val1 = await promise1
+  console.log('Subrato')
+  console.log(val1)
+
+  const val2 = await promise2
+  console.log('Mouli')
+  console.log(val2)
 }
 
 getData()
